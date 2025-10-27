105ff8db5012f462b96a6433662edafa9ab894c6
Golo46 <Golo46@golo46.de>
2025-10-27 15:37:15 +0100

    überarbeitung LOgin und Registrierung

--- src/main/java/com/rezeptapp/data/api/UserManager.java
+++ src/main/java/com/rezeptapp/data/api/UserManager.java
@@ -3,6 +3,6 @@ package com.rezeptapp.data.api;
 public interface UserManager {
     String loginUser(String email, String password);
     boolean logoffUser(String email);
-    boolean registerUser(User user);
+    boolean registerUser(User username);
     
 }
