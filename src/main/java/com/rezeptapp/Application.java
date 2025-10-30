3b6fe559c1faee67f80cfce172ccdbc6aa01f248
Golo46 <Golo46@golo46.de>
2025-10-30 12:29:21 +0100

    build: include Postgres implementations in production component scan; tests use stubs

--- src/main/java/com/rezeptapp/Application.java
+++ src/main/java/com/rezeptapp/Application.java
@@ -3,16 +3,9 @@ package com.rezeptapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
-@ComponentScan(
-        basePackages = "com.rezeptapp",
-        excludeFilters = @ComponentScan.Filter(
-                type = FilterType.REGEX,
-                pattern = "com\\.rezeptapp\\.data\\.implemented\\.Postgres.*"
-        )
-)
+@ComponentScan(basePackages = "com.rezeptapp")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
