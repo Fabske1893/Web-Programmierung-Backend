a402c6840a0bf788818fd761c23577dfa46f361e
Golo46 <Golo46@golo46.de>
2025-11-02 12:17:16 +0100

    cors datei delete

--- src/main/java/com/rezeptapp/config/CorsConfig.java
+++ src/main/java/com/rezeptapp/config/CorsConfig.java
@@ -14,9 +14,11 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://enigmatic-plateau-04468-3ab96016f4f2.herokuapp.com")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*");
+                        .allowedOriginPatterns("*")  
+                        .allowedMethods("*")         
+                        .allowedHeaders("*")         
+                        .exposedHeaders("*")         
+                        .allowCredentials(false);    
             }
         };
     }
